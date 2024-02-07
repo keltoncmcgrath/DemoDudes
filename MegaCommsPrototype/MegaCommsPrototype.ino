ea3ae3e8cce1cce364a17f0035ae506e3789bc3e
keltoncmcgrath <62409451+keltoncmcgrath@users.noreply.github.com>
2024-02-06 23:00:30 -0700

    Implemented wireless communication. Data successfully transfers from uno to mega via xbee. block_data array still needs dynamic memory to work properly.

--- MegaCommsPrototype/MegaCommsPrototype.ino
+++ MegaCommsPrototype/MegaCommsPrototype.ino
@@ -1,7 +1,19 @@
-// Ideal block locs
+// Block Data Storage
 char red1[] = {};
 char blue[] = {};
-char blue[] = {};
+char yellow[] = {};
+char* block_data;
+
+// Variables
+int rc;
+int num_blocks;
+int flag = 33;
+bool data_ready = false;
+
+// Functions
+void fill_devices(size_t size){
+  block_data = malloc(size * sizeof(block_data));
+}
 
 
 void setup() {
@@ -12,6 +24,39 @@ void setup() {
 
 
 void loop() {
-  // TODO
+  // Check for start flag
+  if(!data_ready){
+    if(Serial3.available()>=2){                   // Check if theres data to read
+      if(Serial3.read() == flag){                 // Check for start flag
+        num_blocks = Serial3.read()-'0';          // Store number of blocks
+        data_ready = true;
+      }
+    }
+  }
+
+  // Read data when it's in buffer
+  if(data_ready){               
+    if(Serial3.available()>=num_blocks*4){
+      // char block_data[num_blocks*4] = {};       // Initialize block_data array
+      block_data = (char*)calloc(num_blocks*4, sizeof(char));
+      for(int i=0; i<num_blocks; i++){          // Save data for each block
+        block_data[i*4+0] = Serial3.read();
+        block_data[i*4+1] = Serial3.read();
+        block_data[i*4+2] = Serial3.read();
+        block_data[i*4+3] = Serial3.read();
+      }
+      Serial.write(block_data);
+      data_ready = false;
+    }
+  }
+
+  // Dump buffer
+  while(Serial3.available()){
+    Serial3.read();
+  }
+  // if(Serial3.available()){
+  //   rc = Serial3.read();
+  //   Serial.print(rc);
+  // }
 
 }
--- UnoCommsPrototype/UnoCommsPrototype.ino
+++ UnoCommsPrototype/UnoCommsPrototype.ino
@@ -4,56 +4,60 @@
 // Serial
 SoftwareSerial mySerial(2, 3);  //TX & RX
 
-// Input Data
-int flag = 255;
-int num_blocks = 0;
-
+// Data and Variables
+int flag = 33;
+int num_blocks;
 
 
 void setup() {
   // Begin serial comms
-  Serial.begin(9600);     // USB Port
-  mySerial.begin(9600);   // XBee
+  Serial.begin(9600);    // USB Port
+  mySerial.begin(9600);  // XBee
 
   // Read num blocks on chassis
   Serial.println(F("How Many Blocks on Chassis?"));
-  while(true){                                        // Wait for user input of number of blocks
-    if(Serial.available() == 2) {
-      num_blocks = Serial.read() - '0';               // Serial.read() reads char into an ascii value, subtract ascii for '0' to convert to actual num
+  while (true) {  // Wait for user input of number of blocks (atoi if read as char)
+    if (Serial.available() == 2) {
+      num_blocks = Serial.read() - '0';  // Serial.read() reads char into an ascii value, subtract ascii for '0' to convert to actual num
       Serial.read();
       break;
     }
   }
 
   // Read block data from serial monitor
-  char block_data[4*num_blocks+1] = {};       // Create array to store block data
-  for(int i=0; i<num_blocks; i++){            // Loop over number of blockes reported
+  char block_data[4 * num_blocks + 4] = {};  // Create array to store block data
+  block_data[0] = flag;
+  for (int i = 0; i < num_blocks; i++) {  // Loop over number of blockes reported
     Serial.print(F("Info of Block "));
-    Serial.print(i+1);
+    Serial.print(i + 1);
     Serial.println(F(":"));
-    while(true){                              // Wait for user input of block data
-      if(Serial.available() == 5){            // Read data if available
-        block_data[i*4+0] = Serial.read();
-        block_data[i*4+1] = Serial.read();
-        block_data[i*4+2] = Serial.read();
-        block_data[i*4+3] = Serial.read();
+    while (true) {                    // Wait for user input of block data
+      if (Serial.available() == 5) {  // Read data if available
+        block_data[i * 4 + 1] = Serial.read();
+        block_data[i * 4 + 2] = Serial.read();
+        block_data[i * 4 + 3] = Serial.read();
+        block_data[i * 4 + 4] = Serial.read();
         Serial.read();
         break;
       }
     }
-    if(i==num_blocks-1){                      // Add 'null character' at last index to signal end of array
-        block_data[i*4+4] = "\0";
-      }  
+    if (i == num_blocks - 1) {  // Add 'null character' at last index to signal end of array
+      block_data[i * 4 + 5] = '\0';
+    }
   }
 
-
   // Send block data to mega
-  mySerial.write(block_data);
-  Serial.write(block_data);
+  for (int i = 0; i < num_blocks * 4 + 3; i++) {
+    mySerial.write(block_data[i]);
+    Serial.write(block_data[i]);
+    if (i == 0) {
+      mySerial.write(num_blocks + '0');
+      Serial.write(num_blocks + '0');
+    }
+  }
 }
 
 
 void loop() {
   // TODO
-  
 }
