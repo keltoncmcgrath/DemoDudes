9ee979623887205e9cd2f5a27faee6bbf7c65e40
keltoncmcgrath <62409451+keltoncmcgrath@users.noreply.github.com>
2024-03-01 17:20:19 -0700

    Changes msde to pin_test script for ease of use.

--- TestScripts/pin_test/pin_test.ino
+++ TestScripts/pin_test/pin_test.ino
@@ -1,9 +1,10 @@
-uint8_t pins[] = {24, 25, 26, 27, 28, 29, 30, 31};
+uint8_t pins[] = {24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35};
+int num_pins = 12;
 int data;
 
 void setup() {
   // put your setup code here, to run once:
-  for(int i=0; i<8; i++){
+  for(int i=0; i<num_pins; i++){
     Serial.begin(9600);
     pinMode(pins[i], INPUT_PULLUP);
   }
@@ -11,7 +12,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for(int i = 0; i<8; i++){
+  for(int i = 0; i<num_pins; i++){
     data = digitalRead(pins[i]);
     Serial.print(data);
     Serial.print("\t");
